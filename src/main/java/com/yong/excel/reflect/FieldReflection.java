0ea1dc82e61139aabe1a5a61a7642bedb226dbe6
ChangYong <lcy9002@naver.com>
2017-07-20 21:21:12 +0900

    issue resolve

--- src/main/java/com/yong/excel/reflect/FieldReflection.java
+++ src/main/java/com/yong/excel/reflect/FieldReflection.java
@@ -4,10 +4,8 @@ import com.yong.excel.annotation.Excel;
 import lombok.AllArgsConstructor;
 
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
@@ -16,16 +14,20 @@ import static java.util.stream.Collectors.toList;
  * Created by lichking on 2017. 7. 15..
  */
 public class FieldReflection {
+    private static Map<Class<?>, List<PriorityExcel>> map = new ConcurrentHashMap<>();
+
     public List<String> extractHeadNames(Object source){
-        return extractDatas(source, field -> {
+        List<PriorityExcel> priorityExcels = readClassAndCached(source, field -> {
             Excel annotation = field.getAnnotation(Excel.class);
             String head = annotation.head();
             return new PriorityExcel(annotation.priority(), head.length() < 1 ? field.getName() : head);
         });
+
+        return extractDatas(priorityExcels);
     }
 
     public List<String> extractCells(Object source){
-        return extractDatas(source, field -> {
+        List<PriorityExcel> priorityExcels = readClassAndCached(source, field -> {
             try {
                 field.setAccessible(true);
                 return new PriorityExcel(field.getAnnotation(Excel.class).priority(), field.get(source));
@@ -33,19 +35,30 @@ public class FieldReflection {
                 throw new RuntimeException(e);
             }
         });
+
+        return extractDatas(priorityExcels);
     }
 
-    private List<String> extractDatas(Object source, Function<Field, PriorityExcel> function){
-        Class<?> aClass = source.getClass();
-        Field[] fields = aClass.getDeclaredFields();
-
-        return Arrays.stream(fields)
-                .filter(filed -> filed.isAnnotationPresent(Excel.class))
-                .map(function::apply)
-                .sorted(Comparator.comparingInt(excel -> excel.priority))
+    private List<String> extractDatas(List<PriorityExcel> priorityExcels){
+        return priorityExcels.stream()
                 .map(excel -> excel.data)
                 .map(this::toString)
                 .collect(toList());
+    }
+
+    private List<PriorityExcel> readClassAndCached(Object source, Function<Field, PriorityExcel> function){
+        Class<?> aClass = source.getClass();
+
+        return map.computeIfAbsent(aClass, aClass1 -> {
+            Field[] fields = aClass1.getDeclaredFields();
+            System.out.println("test");
+
+            return Arrays.stream(fields)
+                    .filter(filed -> filed.isAnnotationPresent(Excel.class))
+                    .map(function::apply)
+                    .sorted(Comparator.comparingInt(excel -> excel.priority))
+                    .collect(toList());
+        });
     }
 
     private String toString(Object source){
