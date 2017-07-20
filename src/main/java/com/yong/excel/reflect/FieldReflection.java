67094f3eaf646c107672af29918cf4a2da7f21c3
ChangYong <lcy9002@naver.com>
2017-07-20 20:47:49 +0900

    annotation priority

--- src/main/java/com/yong/excel/reflect/FieldReflection.java
+++ src/main/java/com/yong/excel/reflect/FieldReflection.java
@@ -1,9 +1,11 @@
 package com.yong.excel.reflect;
 
 import com.yong.excel.annotation.Excel;
+import lombok.AllArgsConstructor;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
@@ -16,8 +18,9 @@ import static java.util.stream.Collectors.toList;
 public class FieldReflection {
     public List<String> extractHeadNames(Object source){
         return extractDatas(source, field -> {
-            String head = field.getAnnotation(Excel.class).head();
-            return head.length() < 1 ? field.getName() : head;
+            Excel annotation = field.getAnnotation(Excel.class);
+            String head = annotation.head();
+            return new PriorityExcel(annotation.priority(), head.length() < 1 ? field.getName() : head);
         });
     }
 
@@ -25,25 +28,33 @@ public class FieldReflection {
         return extractDatas(source, field -> {
             try {
                 field.setAccessible(true);
-                return field.get(source);
+                return new PriorityExcel(field.getAnnotation(Excel.class).priority(), field.get(source));
             } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         });
     }
 
-    private List<String> extractDatas(Object source, Function<Field, ?> function){
+    private List<String> extractDatas(Object source, Function<Field, PriorityExcel> function){
         Class<?> aClass = source.getClass();
         Field[] fields = aClass.getDeclaredFields();
 
         return Arrays.stream(fields)
                 .filter(filed -> filed.isAnnotationPresent(Excel.class))
                 .map(function::apply)
+                .sorted(Comparator.comparingInt(excel -> excel.priority))
+                .map(excel -> excel.data)
                 .map(this::toString)
                 .collect(toList());
     }
 
     private String toString(Object source){
         return Objects.isNull(source) ? "" : String.valueOf(source);
+    }
+
+    @AllArgsConstructor
+    private static class PriorityExcel{
+        public int priority;
+        public Object data;
     }
 }
